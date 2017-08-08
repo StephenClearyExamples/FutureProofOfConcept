8838aedf8d5b59971b888ae0e8e652d49465dcec
Stephen Cleary <StephenCleary@users.noreply.github.com>
2017-08-08 18:01:06 -0400

    Add missing CA(false).

--- ConsoleApp/ConsoleApp/FuturePlayground/CompilerSupport/FutureHelpers.cs
+++ ConsoleApp/ConsoleApp/FuturePlayground/CompilerSupport/FutureHelpers.cs
@@ -7,7 +7,7 @@ namespace FuturePlayground.CompilerSupport
         public static async Task<object> Box<T>(Task<T> source) => await source.ConfigureAwait(false);
         public static async Task<object> Box(Task source)
         {
-            await source;
+            await source.ConfigureAwait(false);
             return null;
         }
     }
